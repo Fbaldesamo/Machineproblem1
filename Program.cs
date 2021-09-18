a73e9d6985e971e9d35afbcdd2653699757527d9
Fbaldesamo <baldesamofranco@gmail.com>
2021-09-19 00:26:35 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MachineProblem1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int[] userLogin = new int[5];
+            int[] userPass = new int[5];
+            bool[] userValid = new bool[5];
+            
+
+            Console.Write("Username: ");
+            userValid[0] = Int32.TryParse(Console.ReadLine(),out userLogin[0]);
+            userValid[1] = LoginCheck_User(userLogin[0]);
+
+
+            while (userValid[0] == false || userValid[1] == false)
+            {
+                Console.Write("Username: ");
+                userValid[0] = Int32.TryParse(Console.ReadLine(), out userLogin[0]);
+                userValid[1] = LoginCheck_User(userLogin[0]);
+
+            }
+
+
+            
+
+
+
+
+            Console.WriteLine("Password: ");
+
+            userValid[0] = int.TryParse(Console.ReadLine(), out userPass[1]);
+        }
+
+        public static bool LoginCheck_User(int a)
+        {
+            
+            if (a== 2020156520)
+            {
+                return true;
+               
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+            
+            
         }
     }
 }
