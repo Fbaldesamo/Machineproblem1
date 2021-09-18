9c9e8f7fe0c591d39f9884011945dc3dbcad9864
Fbaldesamo <90735692+Fbaldesamo@users.noreply.github.com>
2021-09-19 00:17:39 +0800

    #1

---
+++ MachineProblem1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MachineProblem1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] userLogin = new int[5];
+            int[] userPass = new int[5];
+            bool[] userValid = new bool[5];
+            
+
+            Console.Write("Username: ");
+            userValid[0] = Int32.TryParse(Console.ReadLine(),out userLogin[0]);
+            userValid[1] = LoginCheck_User(userLogin[0]);
+
+
+            while (userValid[0] == false || userValid[1] == false)
+            {
+                Console.Write("Username: ");
+                userValid[0] = Int32.TryParse(Console.ReadLine(), out userLogin[0]);
+                userValid[1] = LoginCheck_User(userLogin[0]);
+
+            }
+
+
+            
+
+
+
+
+            Console.WriteLine("Password: ");
+
+            userValid[0] = int.TryParse(Console.ReadLine(), out userPass[1]);
+        }
+
+        public static bool LoginCheck_User(int a)
+        {
+            
+            if (a== 2020156520)
+            {
+                return true;
+               
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+            
+            
+        }
+    }
+}
